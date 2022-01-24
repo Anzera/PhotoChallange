3e467c69b25bf4c449816789ff2400a2878b2ecc
Anna Frączek <aniabaj@gmail.com>
2022-01-24 10:50:55 +0100

    Add project files.

---
+++ PhotoChallange/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace PhotoChallange
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.pbPicture = new System.Windows.Forms.PictureBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnAdd.Location = new System.Drawing.Point(0, 482);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(1027, 81);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Dodaj zdjęcie";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // pbPicture
+            // 
+            this.pbPicture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbPicture.Location = new System.Drawing.Point(19, 19);
+            this.pbPicture.Name = "pbPicture";
+            this.pbPicture.Size = new System.Drawing.Size(996, 376);
+            this.pbPicture.TabIndex = 1;
+            this.pbPicture.TabStop = false;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnRemove.Location = new System.Drawing.Point(0, 401);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(1027, 81);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Usuń zdjęcie";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Visible = false;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 563);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.pbPicture);
+            this.Controls.Add(this.btnAdd);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PhotoChallange";
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.PictureBox pbPicture;
+        private System.Windows.Forms.Button btnRemove;
+    }
+}
+
---
+++ PhotoChallange/Form1.cs
@@ -0,0 +1,74 @@
+﻿using PhotoChallange.Properties;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PhotoChallange
+{
+    public partial class Form1 : Form
+    {
+        public string PhotoPath
+        {
+            get
+            {
+                return Settings.Default.PhotoPath;
+            }
+            set
+            {
+                Settings.Default.PhotoPath = value;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            SetPhotoAtStart();
+        }
+        private void SetPhotoAtStart()
+        {
+            if (PhotoPath != "brak")
+            {
+                pbPicture.Image = Image.FromFile(PhotoPath);
+                pbPicture.SizeMode = PictureBoxSizeMode.StretchImage;
+                RemoveBtnVisible(true);
+            }
+            else
+                pbPicture.Image = null;
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            ChoosePicture(); 
+        }
+
+        private void ChoosePicture()
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Image Files(*.jpg;*.png;*.jpeg;*.bmp;*.gif)|*.jpg;*.png;*.jpeg;*.bmp;*.gif";
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                pbPicture.Image = new Bitmap(openFile.FileName);
+                pbPicture.SizeMode = PictureBoxSizeMode.StretchImage;
+                RemoveBtnVisible(true);
+                SaveSettingsData(openFile.FileName);
+            }    
+        }
+
+        private void SaveSettingsData(string data)
+        {
+            Settings.Default["PhotoPath"] = data;
+            Settings.Default.Save();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            pbPicture.Image = null;
+            SaveSettingsData("brak");
+            RemoveBtnVisible(false); 
+        }
+
+        private void RemoveBtnVisible(bool btnVisible)
+        {
+            btnRemove.Visible = btnVisible;
+            btnAdd.Visible = !btnVisible;
+        }
+    }
+}
